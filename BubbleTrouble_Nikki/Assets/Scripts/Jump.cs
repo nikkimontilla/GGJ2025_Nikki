e83c9ab34edfbdc759de8bb8b35b56a3e46468e2
nikkimontilla <francine.montilla@lethpolytech.ca>
2025-01-25 16:27:30 -0700

    Player Speed and Jump Complete

--- BubbleTrouble_Nikki/Assets/Scripts/Jump.cs
+++ BubbleTrouble_Nikki/Assets/Scripts/Jump.cs
@@ -1,16 +1,39 @@
+using System;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Jump : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [SerializeField] private InputActionReference jumpInput;
+    [SerializeField] private float jumpForce = 200f;
+    private CharacterController character;
+    private Vector3 velocity;
+    private float gravity = -9.81f;
+
     void Start()
     {
-        
+        character = GetComponent<CharacterController>();
+        jumpInput.action.Enable();
+        jumpInput.action.performed += Jumping;
     }
 
-    // Update is called once per frame
+    void Jumping(InputAction.CallbackContext context)
+    {
+        if (character.isGrounded)
+        {
+            velocity = Vector3.zero; // Reset velocity
+            velocity.y = jumpForce;
+        }
+    }
+
     void Update()
     {
-        
+        if (character.isGrounded && velocity.y < 0)
+        {
+            velocity.y = 0f;
+        }
+
+        velocity.y += gravity * Time.deltaTime;
+        character.Move(velocity * Time.deltaTime);
     }
 }
