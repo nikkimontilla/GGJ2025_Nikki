b8c9c3ce91805f99dccd359aa2f46d387c0b4d97
nikkimontilla <francine.montilla@lethpolytech.ca>
2025-01-26 14:03:37 -0700

    Bubble Elevator Complete

--- BubbleTrouble_Nikki/Assets/Scripts/BubbleEvelator.cs
+++ BubbleTrouble_Nikki/Assets/Scripts/BubbleEvelator.cs
@@ -1,16 +1,46 @@
 using UnityEngine;
 
-public class BubbleEvelator : MonoBehaviour
+public class BubbleElevator : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [SerializeField] private float floatSpeed = 10f;
+    [SerializeField] private float maxHeight = 50f;
+    [SerializeField] private KeypadController keypad;
+
+    private bool isFloating = false;
+    private BoxCollider triggerCollider;
+    private BoxCollider platformCollider;
+
     void Start()
     {
-        
+        // Add two box colliders
+        triggerCollider = gameObject.AddComponent<BoxCollider>();
+        platformCollider = gameObject.AddComponent<BoxCollider>();
+
+        // Set up trigger collider
+        triggerCollider.isTrigger = true;
+        triggerCollider.size = new Vector3(1.2f, 0.5f, 1.2f); // Slightly larger
+
+        // Set up platform collider
+        platformCollider.size = new Vector3(1f, 0.1f, 1f);
     }
 
-    // Update is called once per frame
+    private void OnTriggerEnter(Collider other)
+    {
+        if (keypad != null && keypad.HasUsedCorrectCode)
+        {
+            isFloating = true;
+        }
+    }
+
     void Update()
     {
-        
+        if (isFloating)
+        {
+            transform.Translate(Vector3.up * floatSpeed * Time.deltaTime);
+            if (transform.position.y >= maxHeight)
+            {
+                isFloating = false;
+            }
+        }
     }
 }
